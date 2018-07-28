e19871c6e3c47b358b714980f9a61e1d8d459f94
Arne De Smedt <arne.desmedt@combellgroup.com>
2018-07-29 00:22:19 +0200

    wrong indentation for test App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
-    <Pagination
-        :totalItems="200"
-        :maxVisiblePages="4"
-        :startPage="3"
-    >
-      <template slot-scope="props">
-        Items {{ props.range.start }} tot {{ props.range.end }} van de {{ props.range.total }}
-      </template>
-    </Pagination>
-  </div>
+    <div id="app">
+        <Pagination
+            :total-items="200"
+            :max-visible-pages="4"
+            :start-page="3"
+        >
+            <template slot-scope="props">
+                Items {{ props.range.start }} tot {{ props.range.end }} van de {{ props.range.total }}
+            </template>
+        </Pagination>
+    </div>
 </template>
 
 <script>
